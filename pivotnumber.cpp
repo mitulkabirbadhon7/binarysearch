39f96a1ea12ff9d83fe24063331953fbe18527b6
MD Mitul Kabir Badhon <126774549+mitulkabirbadhon7@users.noreply.github.com>
2023-09-22 00:01:41 +0600

    Add files via upload

---
+++ pivotnumber.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int getPivot(int arr[],int n){
+    int s = 0;
+    int e = n-1;
+    int mid = s +(e-s)/2;
+
+    while(s<e){
+        if(arr[0]<arr[mid]){
+            s = mid +1;
+        }
+        else {
+            e = mid;
+        }
+        mid = s + (e - s)/2;
+    }
+    return s;
+}
+
+int main(){
+    int arr[5]={7,9,1,2,3};
+    cout<<"pivot is "<<getPivot(arr,5)<<endl;
+}
